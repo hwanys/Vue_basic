2170bce30242654439ada8b0ba5dafa297687e1a
hwanys <ksh7353@naver.com>
2023-11-27 10:14:59 +0900

    remove & toggle

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        footer
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoFooter',
+
+    data() {
+        return {
+            
+        };
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
